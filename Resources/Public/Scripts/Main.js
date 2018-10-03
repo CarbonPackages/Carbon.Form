38b43cec3cbb4dbd3d7f35e5c805858b8315bb3e
Jon Uhlmann <me@jonnitto.ch>
2018-10-03 21:04:39 +0200

    :truck: Rename Package

--- Resources/Public/Scripts/Main.js
+++ Resources/Public/Scripts/Main.js
@@ -1,7 +1,7 @@
 /*!
- * Carbon.FormComponents - created by Jon Uhlmann
- * @build 2018-09-19 13:20
- * @link https://github.com/CarbonPackages/Carbon.FormComponents
+ * Carbon.Form - created by Jon Uhlmann
+ * @build 2018-10-03 20:50
+ * @link https://github.com/CarbonPackages/Carbon.Form
  */
 !function(){"use strict";document.addEventListener("change",function(e){var t=e.target;t&&t.matches(".form-element-field")&&t.classList[t.value?"add":"remove"]("-hasvalue")})}();
 
